d5fbcf4ad1a04781c9e1da7da7b11dac0d172df6
kobreq <gasteinbachs@gmail.com>
2024-10-08 23:56:35 -0300

    Gitignore should now be working

--- target/debug/deps/rust_bubblesort-b4085788987095fb.d
+++
@@ -1,5 +0,0 @@
-/home/kobreq/rust-bubblesort/target/debug/deps/librust_bubblesort-b4085788987095fb.rmeta: src/main.rs
-
-/home/kobreq/rust-bubblesort/target/debug/deps/rust_bubblesort-b4085788987095fb.d: src/main.rs
-
-src/main.rs:
